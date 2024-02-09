b31423bc09046cd64c8b7d20b7f12e15ab405133
Ron Shalom Itsiq <ronshalom.itsiq@post.runi.ac.il>
2024-02-09 08:56:14 +0200

    Implemented the openDB method

--- src/main/java/smarticulous/Smarticulous.java
+++ src/main/java/smarticulous/Smarticulous.java
@@ -23,71 +23,210 @@ public class Smarticulous {
     /**
      * Open the {@link Smarticulous} SQLite database.
      * <p>
-     * This should open the database, creating a new one if necessary, and set the {@link #db} field
+     * This should open the database, creating a new one if necessary, and set the
+     * {@link #db} field
      * to the new connection.
      * <p>
-     * The open method should make sure the database contains the following tables, creating them if necessary:
+     * The open method should make sure the database contains the following tables,
+     * creating them if necessary:
      *
      * <table>
-     *   <caption><em>Table name: <strong>User</strong></em></caption>
-     *   <tr><th>Column</th><th>Type</th></tr>
-     *   <tr><td>UserId</td><td>Integer (Primary Key)</td></tr>
-     *   <tr><td>Username</td><td>Text</td></tr>
-     *   <tr><td>Firstname</td><td>Text</td></tr>
-     *   <tr><td>Lastname</td><td>Text</td></tr>
-     *   <tr><td>Password</td><td>Text</td></tr>
+     * <caption><em>Table name: <strong>User</strong></em></caption>
+     * <tr>
+     * <th>Column</th>
+     * <th>Type</th>
+     * </tr>
+     * <tr>
+     * <td>UserId</td>
+     * <td>Integer (Primary Key)</td>
+     * </tr>
+     * <tr>
+     * <td>Username</td>
+     * <td>Text</td>
+     * </tr>
+     * <tr>
+     * <td>Firstname</td>
+     * <td>Text</td>
+     * </tr>
+     * <tr>
+     * <td>Lastname</td>
+     * <td>Text</td>
+     * </tr>
+     * <tr>
+     * <td>Password</td>
+     * <td>Text</td>
+     * </tr>
      * </table>
      *
      * <p>
      * <table>
-     *   <caption><em>Table name: <strong>Exercise</strong></em></caption>
-     *   <tr><th>Column</th><th>Type</th></tr>
-     *   <tr><td>ExerciseId</td><td>Integer (Primary Key)</td></tr>
-     *   <tr><td>Name</td><td>Text</td></tr>
-     *   <tr><td>DueDate</td><td>Integer</td></tr>
+     * <caption><em>Table name: <strong>Exercise</strong></em></caption>
+     * <tr>
+     * <th>Column</th>
+     * <th>Type</th>
+     * </tr>
+     * <tr>
+     * <td>ExerciseId</td>
+     * <td>Integer (Primary Key)</td>
+     * </tr>
+     * <tr>
+     * <td>Name</td>
+     * <td>Text</td>
+     * </tr>
+     * <tr>
+     * <td>DueDate</td>
+     * <td>Integer</td>
+     * </tr>
      * </table>
      *
      * <p>
      * <table>
-     *   <caption><em>Table name: <strong>Question</strong></em></caption>
-     *   <tr><th>Column</th><th>Type</th></tr>
-     *   <tr><td>ExerciseId</td><td>Integer</td></tr>
-     *   <tr><td>QuestionId</td><td>Integer</td></tr>
-     *   <tr><td>Name</td><td>Text</td></tr>
-     *   <tr><td>Desc</td><td>Text</td></tr>
-     *   <tr><td>Points</td><td>Integer</td></tr>
+     * <caption><em>Table name: <strong>Question</strong></em></caption>
+     * <tr>
+     * <th>Column</th>
+     * <th>Type</th>
+     * </tr>
+     * <tr>
+     * <td>ExerciseId</td>
+     * <td>Integer</td>
+     * </tr>
+     * <tr>
+     * <td>QuestionId</td>
+     * <td>Integer</td>
+     * </tr>
+     * <tr>
+     * <td>Name</td>
+     * <td>Text</td>
+     * </tr>
+     * <tr>
+     * <td>Desc</td>
+     * <td>Text</td>
+     * </tr>
+     * <tr>
+     * <td>Points</td>
+     * <td>Integer</td>
+     * </tr>
      * </table>
-     * In this table the combination of ExerciseId and QuestionId together comprise the primary key.
+     * In this table the combination of ExerciseId and QuestionId together comprise
+     * the primary key.
      *
      * <p>
      * <table>
-     *   <caption><em>Table name: <strong>Submission</strong></em></caption>
-     *   <tr><th>Column</th><th>Type</th></tr>
-     *   <tr><td>SubmissionId</td><td>Integer (Primary Key)</td></tr>
-     *   <tr><td>UserId</td><td>Integer</td></tr>
-     *   <tr><td>ExerciseId</td><td>Integer</td></tr>
-     *   <tr><td>SubmissionTime</td><td>Integer</td></tr>
+     * <caption><em>Table name: <strong>Submission</strong></em></caption>
+     * <tr>
+     * <th>Column</th>
+     * <th>Type</th>
+     * </tr>
+     * <tr>
+     * <td>SubmissionId</td>
+     * <td>Integer (Primary Key)</td>
+     * </tr>
+     * <tr>
+     * <td>UserId</td>
+     * <td>Integer</td>
+     * </tr>
+     * <tr>
+     * <td>ExerciseId</td>
+     * <td>Integer</td>
+     * </tr>
+     * <tr>
+     * <td>SubmissionTime</td>
+     * <td>Integer</td>
+     * </tr>
      * </table>
      *
      * <p>
      * <table>
-     *   <caption><em>Table name: <strong>QuestionGrade</strong></em></caption>
-     *   <tr><th>Column</th><th>Type</th></tr>
-     *   <tr><td>SubmissionId</td><td>Integer</td></tr>
-     *   <tr><td>QuestionId</td><td>Integer</td></tr>
-     *   <tr><td>Grade</td><td>Real</td></tr>
+     * <caption><em>Table name: <strong>QuestionGrade</strong></em></caption>
+     * <tr>
+     * <th>Column</th>
+     * <th>Type</th>
+     * </tr>
+     * <tr>
+     * <td>SubmissionId</td>
+     * <td>Integer</td>
+     * </tr>
+     * <tr>
+     * <td>QuestionId</td>
+     * <td>Integer</td>
+     * </tr>
+     * <tr>
+     * <td>Grade</td>
+     * <td>Real</td>
+     * </tr>
      * </table>
-     * In this table the combination of SubmissionId and QuestionId together comprise the primary key.
+     * In this table the combination of SubmissionId and QuestionId together
+     * comprise the primary key.
+     * 
+     * /**
+     * Opens the SQLite database with the given JDBC URL. It creates the database if
+     * it does not exist and
+     * ensures that the required tables are created.
      *
-     * @param dburl The JDBC url of the database to open (will be of the form "jdbc:sqlite:...")
+     * @param dburl The JDBC url of the database to open (will be of the form
+     *              "jdbc:sqlite:...")
      * @return the new connection
      * @throws SQLException
      */
     public Connection openDB(String dburl) throws SQLException {
-        // TODO: Implement
-        return null;
-    }
+        // Get a connection to the database
+        db = DriverManager.getConnection(dburl);
 
+        // Define SQL statements for creating tables if they do not exist
+        String createUserTableSQL = "CREATE TABLE IF NOT EXISTS User (" +
+                "UserId INTEGER PRIMARY KEY," +
+                "Username TEXT UNIQUE," +
+                "Firstname TEXT," +
+                "Lastname TEXT," +
+                "Password TEXT" +
+                ");";
+
+        String createExerciseTableSQL = "CREATE TABLE IF NOT EXISTS Exercise (" +
+                "ExerciseId INTEGER PRIMARY KEY," +
+                "Name TEXT," +
+                "DueDate INTEGER" +
+                ");";
+
+        String createQuestionTableSQL = "CREATE TABLE IF NOT EXISTS Question (" +
+                "ExerciseId INTEGER," +
+                "QuestionId INTEGER," +
+                "Name TEXT," +
+                "Desc TEXT," +
+                "Points INTEGER," +
+                "PRIMARY KEY (ExerciseId, QuestionId)" +
+                ");";
+
+        String createSubmissionTableSQL = "CREATE TABLE IF NOT EXISTS Submission (" +
+                "SubmissionId INTEGER PRIMARY KEY," +
+                "UserId INTEGER," +
+                "ExerciseId INTEGER," +
+                "SubmissionTime INTEGER" +
+                ");";
+
+        String createQuestionGradeTableSQL = "CREATE TABLE IF NOT EXISTS QuestionGrade (" +
+                "SubmissionId INTEGER," +
+                "QuestionId INTEGER," +
+                "Grade REAL," +
+                "PRIMARY KEY (SubmissionId, QuestionId)" +
+                ");";
+
+        // Execute the SQL statements to create the tables
+        try (Statement stmt = db.createStatement()) {
+            stmt.executeUpdate(createUserTableSQL);
+            stmt.executeUpdate(createExerciseTableSQL);
+            stmt.executeUpdate(createQuestionTableSQL);
+            stmt.executeUpdate(createSubmissionTableSQL);
+            stmt.executeUpdate(createQuestionGradeTableSQL);
+
+        } catch (SQLException e) {
+            // Close the connection if an error occurs during table creation
+            if (db != null)
+                db.close();
+            throw e; // throw the exception
+        }
+        // Return the open connection
+        return db;
+    }
 
     /**
      * Close the DB if it is open.
@@ -106,7 +245,8 @@ public class Smarticulous {
     /**
      * Add a user to the database / modify an existing user.
      * <p>
-     * Add the user to the database if they don't exist. If a user with user.username does exist,
+     * Add the user to the database if they don't exist. If a user with
+     * user.username does exist,
      * update their password and firstname/lastname in the database.
      *
      * @param user
@@ -119,18 +259,20 @@ public class Smarticulous {
         return -1;
     }
 
-
     /**
      * Verify a user's login credentials.
      *
      * @param username
      * @param password
-     * @return true if the user exists in the database and the password matches; false otherwise.
+     * @return true if the user exists in the database and the password matches;
+     *         false otherwise.
      * @throws SQLException
-     * <p>
-     * Note: this is totally insecure. For real-life password checking, it's important to store only
-     * a password hash
-     * @see <a href="https://crackstation.net/hashing-security.htm">How to Hash Passwords Properly</a>
+     *                      <p>
+     *                      Note: this is totally insecure. For real-life password
+     *                      checking, it's important to store only
+     *                      a password hash
+     * @see <a href="https://crackstation.net/hashing-security.htm">How to Hash
+     *      Passwords Properly</a>
      */
     public boolean verifyLogin(String username, String password) throws SQLException {
         // TODO: Implement
@@ -143,14 +285,14 @@ public class Smarticulous {
      * Add an exercise to the database.
      *
      * @param exercise
-     * @return the new exercise id, or -1 if an exercise with this id already existed in the database.
+     * @return the new exercise id, or -1 if an exercise with this id already
+     *         existed in the database.
      * @throws SQLException
      */
     public int addExercise(Exercise exercise) throws SQLException {
         // TODO: Implement
         return -1;
     }
-
 
     /**
      * Return a list of all the exercises in the database.
@@ -182,13 +324,12 @@ public class Smarticulous {
         return -1;
     }
 
-
     // ============= Submission Query ===============
-
 
     /**
      * Return a prepared SQL statement that, when executed, will
-     * return one row for every question of the latest submission for the given exercise by the given user.
+     * return one row for every question of the latest submission for the given
+     * exercise by the given user.
      * <p>
      * The rows should be sorted by QuestionId, and each row should contain:
      * - A column named "SubmissionId" with the submission id.
@@ -196,7 +337,8 @@ public class Smarticulous {
      * - A column named "Grade" with the grade for that question.
      * - A column named "SubmissionTime" with the time of submission.
      * <p>
-     * Parameter 1 of the prepared statement will be set to the User's username, Parameter 2 to the Exercise Id, and
+     * Parameter 1 of the prepared statement will be set to the User's username,
+     * Parameter 2 to the Exercise Id, and
      * Parameter 3 to the number of questions in the given exercise.
      * <p>
      * This will be used by {@link #getLastSubmission(User, Exercise)}
@@ -210,7 +352,8 @@ public class Smarticulous {
 
     /**
      * Return a prepared SQL statement that, when executed, will
-     * return one row for every question of the <i>best</i> submission for the given exercise by the given user.
+     * return one row for every question of the <i>best</i> submission for the given
+     * exercise by the given user.
      * The best submission is the one whose point total is maximal.
      * <p>
      * The rows should be sorted by QuestionId, and each row should contain:
@@ -219,7 +362,8 @@ public class Smarticulous {
      * - A column named "Grade" with the grade for that question.
      * - A column named "SubmissionTime" with the time of submission.
      * <p>
-     * Parameter 1 of the prepared statement will be set to the User's username, Parameter 2 to the Exercise Id, and
+     * Parameter 1 of the prepared statement will be set to the User's username,
+     * Parameter 2 to the Exercise Id, and
      * Parameter 3 to the number of questions in the given exercise.
      * <p>
      * This will be used by {@link #getBestSubmission(User, Exercise)}
@@ -234,10 +378,13 @@ public class Smarticulous {
      * Return a submission for the given exercise by the given user that satisfies
      * some condition (as defined by an SQL prepared statement).
      * <p>
-     * The prepared statement should accept the user name as parameter 1, the exercise id as parameter 2 and a limit on the
-     * number of rows returned as parameter 3, and return a row for each question corresponding to the submission, sorted by questionId.
+     * The prepared statement should accept the user name as parameter 1, the
+     * exercise id as parameter 2 and a limit on the
+     * number of rows returned as parameter 3, and return a row for each question
+     * corresponding to the submission, sorted by questionId.
      * <p>
-     * Return null if the user has not submitted the exercise (or is not in the database).
+     * Return null if the user has not submitted the exercise (or is not in the
+     * database).
      *
      * @param user
      * @param exercise
@@ -271,7 +418,8 @@ public class Smarticulous {
     /**
      * Return the latest submission for the given exercise by the given user.
      * <p>
-     * Return null if the user has not submitted the exercise (or is not in the database).
+     * Return null if the user has not submitted the exercise (or is not in the
+     * database).
      *
      * @param user
      * @param exercise
@@ -282,11 +430,10 @@ public class Smarticulous {
         return getSubmission(user, exercise, getLastSubmissionGradesStatement());
     }
 
-
     /**
      * Return the submission with the highest total grade
      *
-     * @param user the user for which we retrieve the best submission
+     * @param user     the user for which we retrieve the best submission
      * @param exercise the exercise for which we retrieve the best submission
      * @return
      * @throws SQLException
